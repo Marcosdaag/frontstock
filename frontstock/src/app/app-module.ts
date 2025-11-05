a56cf1032bb8275aa907f59a074af3739a6d3431
Marcosdaag <marcosoffs99@gmail.com>
2025-11-04 21:24:51 -0300

    no standalone project and twcss

--- frontstock/src/app/app-module.ts
+++ frontstock/src/app/app-module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Login } from './components/login/login';
+import { Products } from './components/products/products';
+import { Orders } from './components/orders/orders';
+import { Contact } from './components/contact/contact';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Login,
+    Products,
+    Orders,
+    Contact
   ],
   imports: [
     BrowserModule,
