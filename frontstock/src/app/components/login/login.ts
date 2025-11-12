99049febc15fa4c1ff46875c77ec8896baea16e9
Marcosdaag <marcosoffs99@gmail.com>
2025-11-12 07:16:54 -0300

    login form

--- frontstock/src/app/components/login/login.ts
+++ frontstock/src/app/components/login/login.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './login.css',
 })
 export class Login {
+  // 1. Variable para rastrear si la contraseña es visible (por defecto, es falsa/oculta)
+  public passwordVisible: boolean = false;
 
+  constructor() { }
+
+  /**
+   * Método para alternar la visibilidad de la contraseña.
+   */
+  public togglePasswordVisibility(): void {
+    // Simplemente invierte el valor actual de la variable
+    this.passwordVisible = !this.passwordVisible;
+  }
 }
