74df9f6bf6db0e32c014c8c5464ebfde1a133ca6
Marcosdaag <marcosoffs99@gmail.com>
2025-11-12 03:25:35 -0300

    fix contact

--- frontstock/src/app/components/contact/contact.ts
+++ frontstock/src/app/components/contact/contact.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -7,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrl: './contact.css',
 })
 export class Contact {
+  contactForm: FormGroup;
+  enviado = false;
+  cargando = false;
+  mensaje = '';
 
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.invalid) return;
+
+    this.cargando = true;
+    this.mensaje = '';
+
+    const formData = this.contactForm.value;
+
+    // 🔗 tu API deployada en Render
+    this.http.post('https://stockapi-2t3a.onrender.com/contact', formData).subscribe({
+      next: (res: any) => {
+        this.cargando = false;
+        this.enviado = true;
+        this.mensaje = '✅ Mensaje enviado correctamente.';
+        this.contactForm.reset();
+      },
+      error: (err) => {
+        this.cargando = false;
+        this.mensaje = '❌ Error al enviar el mensaje.';
+        console.error(err);
+      },
+    });
+  }
 }
